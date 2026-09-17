e2f274949b91272e1483846fc16c7e15a931d1cf
ramezanivakil <ramazani.un@gmail.com>
2026-09-17 08:58:26 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // ========== آدرس روت جدید کلودفلر روی دامنه اصلی ==========
     // اگر از مسیر اختصاصی استفاده می‌کنید، می‌توانید به صورت 'https://ramezanivakil.ir/api/contact' قرار دهید
-    const WORKER_URL = 'https://ramezanivakil.ir';
+    const WORKER_URL = 'https://ramezanivakil.ir/api/contact';
 
     // ========== دکمه‌های شناور پیمایش ==========
     const scrollUp = document.getElementById('scrollUp');
